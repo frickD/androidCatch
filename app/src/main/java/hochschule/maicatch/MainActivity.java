16484968e34e0f078d40a6df4193e98f0b396f52
juniorrausch <junior.rausch@maihiro.com>
2017-02-10 17:10:57 +0100

    String namen

--- app/src/main/java/hochschule/maicatch/MainActivity.java
+++ app/src/main/java/hochschule/maicatch/MainActivity.java
@@ -2,6 +2,7 @@ package hochschule.maicatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String test;
+    }
+    public void second_layout(View view){
+        setContentView(R.layout.second_layout);
+
+
     }
 }
